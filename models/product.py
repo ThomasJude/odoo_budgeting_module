7e0b85c691b2b6a6ac353138df3c6b09ef71de4c
Anchal <anchal.saxena@oodles.io>
2023-04-06 19:08:56 +0530

    [IMP] worked on product edit to change the invoicing calculation

--- models/product.py
+++ models/product.py
@@ -10,6 +10,74 @@ class ProductTemplate(models.Model):
     product_allocate_budget_line = fields.One2many('product.budget.allocate', 'prod_allocate_id', 'Product Allocate Budget')
     
     
+    
+    def write(self, vals):
+        res = super(ProductTemplate, self).write(vals)
+        print(vals)
+        # for product_fixed_budget_line,val in zip(self.product_fixed_budget_line,vals.get('product_fixed_budget_line')):
+        #     if val[1] != product_fixed_budget_line.id:
+        #         print(val[1], product_fixed_budget_line.id)
+        #         existing_prod_priority = self.env['product.budget.fixed'].sudo().search(
+        #             [('prod_priority', '=', product_fixed_budget_line.prod_priority),('prod_id','=',self.id)])
+        #         print("DDDDVVVVVVVVV",existing_prod_priority,self.id,product_fixed_budget_line.prod_priority)
+        #         if existing_prod_priority:
+        #             raise UserError(_('Product priority should be unique in Fixed Reduction budgeting tab'))
+
+        product_product_id = self.env['product.product'].sudo().search([('product_tmpl_id','=',self.id)])
+        invoices_lines = self.env['account.move.line'].sudo().search([('product_id','=',product_product_id.id)])
+        # print("NNNNNNNNNNNNNNNNNNN",invoices_lines.move_id.state)
+        for invoice in invoices_lines:
+            if invoice.move_id.state == 'draft':
+                for inv_budget_line in invoice.move_id.inv_budget_line:
+                    print('SAADDDDD',inv_budget_line.account_move_line_id,invoice.id)
+                    if inv_budget_line.account_move_line_id.id == invoice.id:
+                        inv_budget_line.unlink()
+                for remain_budget_line in invoice.move_id.product_remaining_budget_line:
+                    print('SAADDDDD',remain_budget_line.account_move_line_id,invoice.id)
+                    if remain_budget_line.account_move_line_id.id == invoice.id:
+                        remain_budget_line.unlink()
+                if invoice.product_id and invoice.product_id.product_tmpl_id and invoice.product_id.product_fixed_budget_line:
+                    for fix_budget_line in self.product_fixed_budget_line:
+                        budget_data = self.env['invoice.budget.line'].sudo().create({
+                            'product_id_budget': fix_budget_line.product_id.id,
+                            'name': fix_budget_line.name,
+                            'prod_inv_id': invoice.move_id.id,
+                            'account_move_line_id': invoice.id,
+                            'bucket_type_id': fix_budget_line.bucket_type_id.id,
+                            'assignable_status': fix_budget_line.assignable_status,
+                            'amount': fix_budget_line.amount * invoice.quantity,
+                            'is_vendor':fix_budget_line.is_vendor,
+                            # 'bucket_user': fix_budget_line.bucket_user,
+                            # 'budget_inv_vendor_ids': [(6,0, fix_budget_line.prod_fix_vendor_ids.ids)] or [],
+                            'budget_inv_vendor_id': fix_budget_line.prod_fix_vendor_id.id,
+                            # 'budget_user_ids':[(6,0, fix_budget_line.prod_fix_assigned_user_ids.ids)] or [],
+                            'budget_user_id': fix_budget_line.prod_fix_assigned_user_id.id,
+                            'prod_priority': fix_budget_line.prod_priority,
+                            # 'fixed_budget_line_id': fix_budget_line.id
+                        })
+                if invoice.product_id and invoice.product_id.product_tmpl_id and invoice.product_id.product_allocate_budget_line:
+                    for allocate_budget_line in self.product_allocate_budget_line:
+                        remaining_budget_data = self.env['product.budget.remaining'].sudo().create({
+                            'product_id_budget': allocate_budget_line.product_id.id,
+                            'name': allocate_budget_line.name,
+                            'prod_remaining_id': invoice.move_id.id,
+                            'account_move_line_id': invoice.id,
+                            
+                            'bucket_type_id': allocate_budget_line.bucket_type_id.id,
+                            'assignable_status': allocate_budget_line.assignable_status,
+                            # 'bucket_user': allocate_budget_line.bucket_user,
+                            'is_vendor':allocate_budget_line.is_vendor,
+                            # 'budget_inv_remaining_vendor_ids': [(6,0, allocate_budget_line.prod_remaining_budget_vendor_ids.ids)] or [],
+                            'budget_inv_remaining_vendor_id': allocate_budget_line.prod_remaining_budget_vendor_id.id,
+                            # 'budget_remaining_user_ids':[(6,0, allocate_budget_line.prod_remaining_budget_assigned_user_ids.ids)] or [],
+                            'budget_remaining_user_id': allocate_budget_line.prod_remaining_budget_assigned_user_id.id,
+                            'allocate_percent': allocate_budget_line.allocate_percent,
+                            'amount': allocate_budget_line.amount * invoice.quantity
+                        })
+
+        return res
+    
+    
     # @api.constrains('product_fixed_budget_line,product_fixed_budget_line.prod_priority')
     # def prod_priority_val(self):
     #     print ("2222222222222222")
@@ -38,6 +106,7 @@ class ProductTemplate(models.Model):
             for lines in self.product_fixed_budget_line:
                 if lines.amount:
                     total += lines.amount
+            self.product_allocate_budget_line._constrains_allocate_percent()
         if total > self.list_price:
             raise UserError(_("Total of Fixed Reductions should not be Greater than Selling Price"))
 
