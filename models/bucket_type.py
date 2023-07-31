ba055f51f2f9d6314a1107e63733edddccaa43b9
deepak <deepakoodles1221>
2023-07-31 11:40:27 +0530

    [add] add validation and bucket view changes

--- models/bucket_type.py
+++ models/bucket_type.py
@@ -12,6 +12,14 @@ class BucketType(models.Model):
     def unlink(self):
         if not self.env.user.has_group('odoo_budgeting_module.bucket_delete_group'):
             raise UserError("You don't have permission to delete this record.")
+        bucket = self.env['bucket'].search([('bucket_type_id','=',self.id)])
+        product_fixed = self.env['product.budget.fixed'].search([('bucket_type_id','=',self.id)])
+        product_allocate = self.env['product.budget.allocate'].search([('bucket_type_id','=',self.id)])
+        allocation_template = self.env['allocation.template.line'].search([('bucket_type','=',self.id)])
+        for rec in bucket:
+            if rec.bucket_amount > 0.0 or len(product_fixed) > 0 or \
+                    len(product_allocate) > 0 or len(allocation_template) > 0:
+                raise UserError("Bucket Type has been used in Invoices/Products/Budget Allocation Templates.")
         return super(BucketType,self).unlink()
 
     @api.constrains('is_vendor')
@@ -22,9 +30,7 @@ class BucketType(models.Model):
             if obj:
                 if record.is_vendor:
                     raise UserError(_('There is already a bucket type exist with Vendor'))
-                
-                
-                
+
     @api.model_create_multi
     def create(self, vals_list):
         res = super(BucketType,self).create(vals_list)
